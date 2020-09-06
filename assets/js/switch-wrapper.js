3ce20ad5c4b202291a0ff5e82e4bc2d60372905a
fishand4ips <alekomegunov@gmail.com>
2020-09-07 00:02:05 +0300

    add night theme

---
+++ assets/js/switch-wrapper.js
@@ -0,0 +1,22 @@
+$(function () {
+    const d = new Date();
+    const hours = d.getHours();
+    const night = hours >= 19 || hours <= 7; // between 7pm and 7am
+    const body = document.querySelector('body');
+    const toggle = document.getElementById('toggle');
+    const input = document.getElementById('switch');
+
+    if (night) {
+        input.checked = true;
+        body.classList.add('night');
+    }
+
+    toggle.addEventListener('click', function () {
+        const isChecked = input.checked;
+        if (isChecked) {
+            body.classList.remove('night');
+        } else {
+            body.classList.add('night');
+        }
+    });
+});
