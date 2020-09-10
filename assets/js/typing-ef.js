79eff2be20d8079fb97d446269e2a279622f6b95
fishand4ips <alekomegunov@gmail.com>
2020-09-10 07:56:35 +0300

    refactor

--- assets/js/typing-ef.js
+++ assets/js/typing-ef.js
@@ -1,28 +1,27 @@
-let wrapper
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+let wrapper;
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function writingAll(stringTarget, container) {
-    wrapper = document.querySelector('[' + container + ']')
-    const stringsContainer = document.getElementsByClassName(stringTarget)
+    wrapper = document.querySelector('[' + container + ']');
+    const stringsContainer = document.getElementsByClassName(stringTarget);
 
     while (wrapper) {
         for (i = 0; i < stringsContainer.length; i++) {
-            const string = stringsContainer[i].textContent
-            await write(string)
-            await sleep(1000)
-            await erase()
+            const string = stringsContainer[i].textContent;
+            await write(string);
+            await sleep(1000);
+            await erase();
             await sleep(1000)
         }
-        ;
     }
-};
+}
 
 async function write(text) {
-    let index = 0
+    let index = 0;
     while (index < text.length) {
-        const timeout = 100
-        await sleep(timeout)
-        index++
+        const timeout = 100;
+        await sleep(timeout);
+        index++;
         wrapper.innerHTML = text.substring(0, index)
     }
 }
@@ -30,8 +29,8 @@ async function write(text) {
 
 async function erase() {
     while (wrapper.textContent.length) {
-        const timeout = 100
-        await sleep(timeout)
+        const timeout = 100;
+        await sleep(timeout);
         wrapper.textContent = wrapper.textContent.substring(0, wrapper.textContent.length - 2)
     }
 }
